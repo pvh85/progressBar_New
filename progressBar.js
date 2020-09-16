55bcb7f121c04787c9291c6da380389d8535ee33
Хомутов Павел Владимирович <p.khomutov@faberlic.com>
2020-09-16 20:21:24 +0300

    fixes

--- progressBar.js
+++ progressBar.js
@@ -1,46 +1,77 @@
 class barWindow {
   //url - адрес куда шлем запросы, на получение данных для баров
   //id - id диалога
-  constructor(id,url) {
+  constructor(id, url) {
     this.id = id
     this.url = url
-    this.bars=[]
-    this.dialogButtons = [
-      {
+    this.bars = []
+    this.dialogButtons = [{
         text: "Cancel",
         click: this.close.bind(this)
       },
       {
-        text:"AddBar",
+        text: "AddBar",
         click: () => {
-          this.addBar(
-            {
-              idItem:this.bars.length+1,
-              position:Math.random(100)*100,
-              count:100,
-              progressText:`autobar:${this.bars.length+1}`
-            }
-          )
+          this.addBar({
+            idItem: this.bars.length + 1,
+            position: Math.random(100) * 100,
+            count: 100,
+            progressText: `autobar:${this.bars.length+1}`
+          })
         },
       },
       {
-        text:"RemoveBar",
+        text: "RemoveBar",
         click: this.removeBar.bind(this)
       },
       {
-        text:"GetByUrl",
+        text: "GetByUrl",
         click: () => {
-          this.getData;
-          this.dataLoop = setInterval(this.getData.bind(this),1000);
+          if (!this.dataLoop) {
+            this.getData()
+            this.dataLoop = setInterval(this.getData.bind(this), 1000);
+          }
         }
-      } 
+      }
     ];
     this.dialogDiv = $(`<div id = ${id} class = "div-300"></div>`);
   }
-  
+
   //Получаем данные и рисуем бары
-  getData(){
+  async getData() {
     console.log('tick')
+    try {
+      let response = await fetch(this.url)
+      if (response.ok) {
+        let data = await response.json();
+        data.progresses.map(elem => {
+          if (this.bars.length == 0) {
+            this.addBar(elem)
+          } else {
+            let barIndex = null
+            /*
+              Проверяет существование бара
+              возможно можно красивее, подумать.
+            */
+            for (let i = 0; i < this.bars.length; i++) {
+              if (elem.idItem == this.bars[i].obj.id) {
+                barIndex = i;
+                break;
+              } else {
+                barIndex = null;
+              }
+            }
+            barIndex != null ? this.bars[barIndex].obj.setValue(elem.position) : this.addBar(elem)
+          }
+        })
+      } else {
+        throw ('bad response ' + response.status)
+      }
+    } catch (exception) {
+      alert(`fail while get data url: ${this.url} 
+           error: ${exception}`)
+    }
+    /* старое
     fetch(this.url)
       .then((response) => {
         if (response.status == 200){
@@ -51,28 +82,11 @@ class barWindow {
         }
       })
       .then((data) => {
-        data.progresses.map(elem => {
-          if (this.bars.length == 0){
-            this.addBar(elem)
-          }
-          else{  
-            let barIndex =null
-            for (let i = 0; i< this.bars.length; i++){
-              if (elem.idItem == this.bars[i].obj.id){
-                barIndex = i;
-                break;
-              }
-              else{
-                barIndex = null;
-              }
-            }
-            barIndex != null ? this.bars[barIndex].obj.setValue(elem.position) : this.addBar(elem)
-          }  
-        })
       })
-     // .catch(function (error) {
-     //   alert(error)
-     // })
+     .catch( (error) => {
+       alert(error)
+     })
+     */
 
   }
 
@@ -80,11 +94,11 @@ class barWindow {
   close() {
     this.dialogBlock.dialog("destroy");
     this.dialogBlock.remove();
-    this.bars.map( elem => {
-      elem.block.map(block=>block.remove())
+    this.bars.map(elem => {
+      elem.block.map(block => block.remove())
     });
     clearInterval(this.dataLoop);
-    this.bars=[]
+    this.bars = []
   }
 
   //рисует диалог 
@@ -94,19 +108,19 @@ class barWindow {
     this.parent.append(this.dialogDiv)
     this.dialogBlock = $("#" + this.id)
     this.dialogBlock.dialog({
-      position:{
-        // my: 'left top',
-        // at: 'left',
-        of:$('#'+this.parent.attr('id'))
-      },
-      modal: false,
-      autoOpen: false,
-      buttons: this.dialogButtons,
-      appendTo: '#'+this.parent.attr('id')
-    })
-    .parent().draggable({
-        containment: '#'+this.parent.attr('id')
-    });
+        position: {
+          // my: 'left top',
+          // at: 'left',
+          of: $('#' + this.parent.attr('id'))
+        },
+        modal: false,
+        autoOpen: false,
+        buttons: this.dialogButtons,
+        appendTo: '#' + this.parent.attr('id')
+      })
+      .parent().draggable({
+        containment: '#' + this.parent.attr('id')
+      });
 
     this.dialogBlock.dialog("open");
   }
@@ -119,20 +133,20 @@ class barWindow {
   //добавляет бар в диалог
   addBar(barData) {
     const bar = new ProgressBar(this.dialogBlock, barData)
-    this.bars.push(
-      {
-        "obj":bar.getObj() ,
-        "block":bar.getBlock()
-      }
-    )
+    this.bars.push({
+      "obj": bar.getObj(),
+      "block": bar.getBlock()
+    })
     bar.open();
   }
 
   // удаляем последний бар из диалога 
   // легко модифицируется для удаления конкретного бара
-  removeBar(barData){
-    if (this.bars.length > 0) {  
-      this.bars[this.bars.length-1].block.map(block => {block.remove()});
+  removeBar(barData) {
+    if (this.bars.length > 0) {
+      this.bars[this.bars.length - 1].block.map(block => {
+        block.remove()
+      });
       this.bars.pop()
     }
   }
@@ -142,7 +156,10 @@ class barWindow {
 
 
 class ProgressBar {
-  constructor(parent, {idItem,...barData}) {
+  constructor(parent, {
+    idItem,
+    ...barData
+  }) {
     this.parent = parent
     this.id = idItem
     this.data = barData
@@ -155,40 +172,40 @@ class ProgressBar {
     this.parent.append(this.barText)
     this.parent.append(this.barBlock)
     this.barBlock.progressbar({
-      classes:{
+      classes: {
         "ui-progressbar": "ui-corner-all",
         "ui-progressbar-complete": "ui-corner-right",
         "ui-progressbar-value": "ui-corner-left"
       },
-      max:this.data.count,
-      value:this.data.position
+      max: this.data.count,
+      value: this.data.position
     })
     this.updateValueText();
   }
 
   //выставляет значение бара
-  setValue(value){
-    this.barBlock.progressbar( "value", value );
+  setValue(value) {
+    this.barBlock.progressbar("value", value);
     this.updateValueText();
   }
 
   //обновление текста прогресса
-  updateValueText(){
-    const newVal = Math.floor(this.barBlock.progressbar( "option", "value" ))
+  updateValueText() {
+    const newVal = Math.floor(this.barBlock.progressbar("option", "value"))
     this.barBlock
       .children('.ui-progressbar-value')
       .html(`<span class='value-text'> ${newVal + '%'} </span>`)
       .css("display", "block");
   }
 
-  
+
   //возвращает jq блок
   getBlock() {
-    return [this.barBlock,this.barText]
+    return [this.barBlock, this.barText]
   }
 
   //возвращает объект
-  getObj(){
+  getObj() {
     return this
   }
 }
--- script.js
+++ script.js
@@ -2,23 +2,19 @@
 //progress bar classes .ui-progressbar, .ui-progressbar-value
 $(document).ready(function () {
 	$('#mainWindow').append('<button id="downloadButton">Start</button>')
-	
+
 	$('#testWindow2').append('<button id="downloadButton2">Start</button>')
 	$('#downloadButton').click(function () {
-	//	createProgressBarWindow('http://localhost:5000/api/progress_test')
-		
-		//createProgressBarWindow('http://localhost:5000/api/progress_test')
-		
-		const diaObj = new barWindow ('13','http://127.0.0.1:5000/api/progress_test/');
+		const diaObj = new barWindow('13', 'http://127.0.0.1:5000/api/progress_test/');
 		diaObj.open($('#mainWindow'));
 
 	})
 	$('#downloadButton2').click(function () {
-	const dd = new barWindow('14','http://127.0.0.1:5000/api/progress_test/');
-	dd.open($('#testWindow2'));
-	return false;
+		const dd = new barWindow('14', 'http://127.0.0.1:5000/api/progress_test/');
+		dd.open($('#testWindow2'));
+		return false;
 	});
-	
+
 	//const barObj = new ProgressBar ('13','12345');
 	//barObj.appendElem('body');
 
